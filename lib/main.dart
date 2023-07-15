4d09eb3d8de6330df3aaa41e7a4eafd0f173019f
ireedui0223 <moogeeireedui@gmail.com>
2023-07-15 18:09:39 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_start/pages/offerpage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: const MyApp(),
+    theme: ThemeData(
+      primarySwatch: Colors.brown,
+    ),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MainApp createState() => MainApp();
+}
+
+class MainApp extends State<MyApp> {
+  String value = "hello world";
+  int selectedIndex = 0;
+
+  void onClickBtn(String inputValue) {
+    setState(() {
+      value = DateTime.now().toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget currentWidgetPage = const Text("!!!");
+
+    switch (selectedIndex) {
+      case 0:
+        currentWidgetPage = const OffersPage();
+        break;
+      case 1:
+        currentWidgetPage = const Text("Shop");
+        break;
+      case 2:
+        currentWidgetPage = const Text("order");
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ireedui beno"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (newIndex) {
+            setState(() {
+              selectedIndex = newIndex;
+            });
+          },
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.yellow.shade400,
+          unselectedItemColor: Colors.yellow.shade50,
+          items: const [
+            BottomNavigationBarItem(label: "Menu", icon: Icon(Icons.coffee)),
+            BottomNavigationBarItem(label: "Shop", icon: Icon(Icons.shop)),
+            BottomNavigationBarItem(
+              label: "Order",
+              icon: Icon(Icons.shopping_cart_checkout_outlined),
+            )
+          ]),
+      body: currentWidgetPage,
+    );
+  }
+}
+
+class Greet extends StatefulWidget {
+  const Greet({super.key});
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+class _GreetState extends State<Greet> {
+  String name = "test";
+
+  @override
+  Widget build(BuildContext context) {
+    const greetStyle = TextStyle(color: Colors.blueAccent);
+
+    return Column(children: <Widget>[
+      Row(
+        children: [
+          Text("$name beeonoo", style: greetStyle),
+          Column(
+            children: [
+              Image.asset(
+                'images/profile.png',
+                height: 200,
+              )
+            ],
+          )
+        ],
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 24),
+        child: TextField(
+          obscureText: true,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+            hintText: 'Enter Password',
+          ),
+          onChanged: (text) {
+            setState(() {
+              name = text;
+            });
+          },
+          style: greetStyle,
+        ),
+      ),
+    ]);
+  }
+}
---
+++ lib/pages/offerpage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class OffersPage extends StatelessWidget {
+  const OffersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion"),
+        Offer(title: "title", descripion: "descripion")
+      ],
+    );
+  }
+}
+
+class Offer extends StatelessWidget {
+  final String title;
+  final String descripion;
+
+  const Offer({
+    super.key,
+    required this.title,
+    required this.descripion,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Card(
+        elevation: 7,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,
+              image: AssetImage("images/profile.png"),
+            ),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                    child: Text(
+                  title,
+                  style: Theme.of(context).textTheme.headline3,
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                    child: Text(
+                  descripion,
+                  style: Theme.of(context).textTheme.headline6,
+                )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
